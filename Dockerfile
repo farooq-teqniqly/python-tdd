3c0da7bf575657d871a52865930ca55e05ed1956
Farooq Mahmud <mahmud.farooq@acuitybrands.com>
2023-07-11 16:01:26 -0400

    Added Postgres db.

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,18 @@
-FROM python:3.10-slim
+FROM python:3.10-slim-buster
 
 WORKDIR /usr/src/app
 
 ENV PYTHONDONTWRITEBYTECODE 1
 ENV PYTHONUNBUFFERED 1
 
+RUN apt-get update \
+  && apt-get -y install netcat gcc postgresql libpq-dev \
+  && apt-get clean
+
 COPY ./requirements.txt .
 RUN pip install -r requirements.txt
 
 COPY . .
 
-CMD python manage.py run -h 0.0.0.0
+COPY ./entrypoint.sh .
+RUN chmod +x /usr/src/app/entrypoint.sh
--- src/config.py
+++ src/config.py
@@ -1,12 +1,16 @@
+import os
+
 class BaseConfig:
     TEST = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = None
 
 class DevelopmentConfig(BaseConfig):
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL_DEVELOPMENT")
 
 class TestConfig(BaseConfig):
     TEST = True
-
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL_TEST")
 
 class ProductionConfig(BaseConfig):
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL_PRODUCTION")
