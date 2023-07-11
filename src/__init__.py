a9f7551c970e3fede6a426bedd86ccc53d69beec
Farooq Mahmud <mahmud.farooq@acuitybrands.com>
2023-07-11 15:33:53 -0400

    Docker support.

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,12 @@
+import os
+
 from flask import  Flask, jsonify
 from flask_restx import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
-app.config.from_object("src.config.DevelopmentConfig")
+app_settings = os.getenv("APP_SETTINGS")
+app.config.from_object(app_settings)
 
 class Ping(Resource):
     def get(self):
